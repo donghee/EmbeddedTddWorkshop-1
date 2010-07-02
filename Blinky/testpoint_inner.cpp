523dc7e60f0c5f34edb658299bdd2e57d5d7af74
Donghee Park <donghee@wally.(none)>
2010-07-02 18:03:04 +0900

    after 2010 07 02 meething

--- Blinky/testpoint_inner.cpp
+++ Blinky/testpoint_inner.cpp
@@ -5,15 +5,18 @@ extern "C" {
 #include "../../ChordedKeyboard/tests/keyevent.h"
 }
 
-#define BUF_SIZE 500
+#define BUF_SIZE 700
 
 int ledPin =  13;    // LED connected to digital pin 13
 
 boolean A;
 int event;
+int event_calls;
+
 byte firstA;
 byte secondA;
 int i=0;
+
 
 char buffer[BUF_SIZE];
 
@@ -22,24 +25,41 @@ void setup()   {
   // initialize the digital pin as an output:
   pinMode(5, INPUT);
   pinMode(ledPin, OUTPUT);
-  Serial.begin(9600);
-  set_uncertain_count(100);
+  Serial.begin(1200);
+  set_uncertain_count(200);
+  digitalWrite(ledPin, HIGH);   // set the LED on
 }
 
 void printBuffer(){
   for (i=0; i< BUF_SIZE ;i++) {
-	Serial.print(1,DEC);	
-	//	Serial.print(buffer[i], DEC);
+	  //Serial.print(1,DEC);
+	  Serial.print(buffer[i], DEC);
   }
 }
 
 int main(void) {
+    unsigned long ctime=0;
+    int k=0;
 
   init();
   setup();
-  for (;;) {
-	  digitalWrite(ledPin, HIGH);   // set the LED on
+  // for (int h=0;h<10;h++) {
+  //     ctime=micros();
+  //     Serial.println(1,DEC);
+  //     Serial.println(micros()-ctime,DEC);
+  // }
+
+  for (;;k++) {
 	  A = digitalRead(5);
+      /*
+      if (A==prev_A) {
+          run_length++;
+      } else {
+          buffer.append(A,run_length);
+          run_length=0;
+      }
+      */
+
 	  if (A == LOW) {
 		  key_step(ON);
 	  }
@@ -47,19 +67,33 @@ int main(void) {
 		  key_step(OFF);
 	  }
 	  event = get_event();
-	  //	  if (event != NOEVT) Serial.print(event, DEC);
+	  if (event != NOEVT) Serial.print(event, DEC);
+      //Serial.println(event,DEC);
 
+      // if (!(k%1000)) {
+      //     k=0;
+      //     Serial.println(micros()-ctime,DEC);
+      //     ctime=micros();
+      // }
+          
+  }
+      /*
+	  buffer[i] = event;
+	  i++;
 	  buffer[i] = A;
 	  i++;
 
 	  if (event == RELEASED) {
-		printBuffer();
+		  event_calls += 1;
+		  if(event_calls == 4) {
+			  printBuffer();
+			  event_calls = 0;
+		  }
 	  }
 
 	  if (i >= BUF_SIZE) {
 		i=0;
 	  }
+	  */
 
-
-  } // end for
 } // end main
--- TestPoint/read_keystep.py
+++ TestPoint/read_keystep.py
@@ -1,12 +1,53 @@
-import serial
+import serial,sys
+filename = 'i.log'
+s=serial.Serial("Com1", 1200)
+f=open(filename,'w')
+event = None
+value = None
+i=0
 
-s=serial.Serial("Com1")
+def linereader(s):
+    b=[]
+    while True:
+        b.append(s.read())
+        if b[-1]=='\n':
+            yield ''.join(b)
+            b=[]
+
+lr=linereader(s)
 try:
-	for i in range(1000):
-	    print "%s" % s.read()
-	    print "=========== %d" % i
+    while True:
+        # event = lr.next()
+        # if int(event) >3 :
+        #     print (event)
+        # continue
+        event = s.read()
+        e = int(event)
+        if e == 0:
+            event = 'NOEVT'
+        elif e == 1:
+            event = 'PRESSED'
+        elif e == 2:
+            event = 'RELEASED'
+        # value = s.read()
+        # f.write("%s, %s\n" % (event, value) )
+        # f.write("========== %d \n" % i)
+        # print "%s, %s\n" % (event, value)
+        if event=='PRESSED':
+            sys.stdout.write('P')
+        if event=='RELEASED':
+            sys.stdout.write('R')
+            sys.stdout.write('%03d'%i)
+            i+=1
+        
+        sys.stdout.flush()
+        #print "=========== %d" % i
 
+        
+        f.flush()
+        
 except:
 	pass
 finally:
+	f.close()
 	s.close()
