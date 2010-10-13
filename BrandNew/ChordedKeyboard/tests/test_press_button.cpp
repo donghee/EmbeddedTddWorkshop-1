5bd986ff482a62d208457a7e94987bac8742238f
Donghee PARK <dongheepark@piny.cc>
2010-10-13 10:33:07 +0900

    arrange new project directory

---
+++ BrandNew/ChordedKeyboard/tests/test_press_button.cpp
@@ -0,0 +1,52 @@
+#include "CppUTest/TestHarness.h"
+#include "CppUTestExt/MockSupport.h"
+
+TEST_GROUP(PressButton)
+{
+};
+
+
+void productionCode()
+{
+    mock().actualCall("productionCode");
+}
+
+TEST(PressButton, SimpleScenario)
+{
+    mock().expectOneCall("productionCode");
+    productionCode();
+    mock().checkExpectations();
+}
+
+// TEST(PressButton, test_press_and_led_on) {
+// 	//when button is pressed
+// 	_button_pressed();
+// 	//check light is on
+// 	expect_call_led_on();
+// }
+
+// TEST(PressButton, test_press_and_led_on) {
+// 	//when started
+// 	_exit_mainloop=1;
+// 	start();
+// 	//loop_step must be called continuously
+// 	expect_call_loop_step();
+// }
+
+// TEST(PressButton, test_press_and_led_on) {
+// 	//when loop_step is called
+// 	loop_step();
+// 	//is_pressed should have been called
+// 	expect_call_is_pressed();
+// 	set_return_value_for_is_pressed(1);
+// 	expect_call_led_on();
+
+// 	loop_step();
+// 	expect_call_is_pressed();
+// 	set_return_value_for_is_pressed(0);
+// 	expect_call_led_off();
+// 	//if is_pressed is true
+// 	   //expect led_on is called
+// 	//if is_pressed is false
+// 	   //expect led_off is called
+// }
---
+++ BrandNew/CppUTest/makeAll.sh
@@ -0,0 +1,5 @@
+#Generated file - makeAll.sh
+echo "Running makeAll for CppUTest v2.2d created on 2010-09-12-14-44"
+export CPPUTEST_HOME=$(pwd)
+echo "export CPPUTEST_HOME=$(pwd)/"
+make everythingInstall
